4afcc98463de2846e110f0ede7af4d6c74bd47ae
Xavier Tondelier <xtondelier@gmail.com>
2025-12-18 21:45:21 +0000

    Add icon for drop-down button

--- src/drop-down.js
+++ src/drop-down.js
@@ -1,11 +1,18 @@
-function createDropDown(name, items) {
+function createDropDown(name, items, menuIcon = null) {
   const dropDownDiv = document.createElement("div");
   dropDownDiv.classList.add("drop-down");
 
   const dropDownBtn = document.createElement("button");
+  const dropDownBtnSpan = document.createElement("span");
   dropDownBtn.classList.add("drop-down-btn");
-  dropDownBtn.textContent = name;
-  dropDownDiv.appendChild(dropDownBtn)
+  dropDownBtnSpan.textContent = name;
+  dropDownBtn.appendChild(dropDownBtnSpan);
+  if (menuIcon !== null) {
+    const dropDownBtnImg = document.createElement("img");
+    dropDownBtnImg.src = menuIcon;
+    dropDownBtn.appendChild(dropDownBtnImg);
+  }
+  dropDownDiv.appendChild(dropDownBtn);
 
   const itemsDiv = document.createElement("div");
   itemsDiv.classList.add("drop-down-items");
@@ -14,12 +21,11 @@ function createDropDown(name, items) {
     itemDiv.classList.add("drop-down-item");
     itemDiv.textContent = item;
 
-    itemsDiv.appendChild(itemDiv)
+    itemsDiv.appendChild(itemDiv);
   }
   dropDownDiv.appendChild(itemsDiv);
 
   return dropDownDiv;
-
 }
 
 export default createDropDown;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import "./style.css";
 import createDropDown from "./drop-down.js";
+import menuIcon from "./images/menu-icon.svg";
 
-const dropDownMenu = createDropDown("Cars", ["Volvo", "Volkswagen", "Porsche"])
+const dropDownMenu = createDropDown(
+  "Cars",
+  ["Volvo", "Volkswagen", "Porsche"],
+  menuIcon
+);
 
-document.body.appendChild(dropDownMenu)
+document.body.appendChild(dropDownMenu);
